2ea18897d0bad903ed578cc5783729b76ebc170c
Thomas <thomas@getlantern.org>
2026-01-28 16:07:51 -0500

    add unique device count metric

--- tracker/metrics/metrics.go
+++ tracker/metrics/metrics.go
@@ -6,9 +6,11 @@
 package metrics
 
 import (
+	"context"
 	"time"
 
 	"github.com/getlantern/geo"
+	"github.com/getlantern/http-proxy-lantern/v2/instrument/distinct"
 	"github.com/sagernet/sing-box/adapter"
 	"github.com/sagernet/sing-box/log"
 	"go.opentelemetry.io/otel"
@@ -18,10 +20,15 @@ import (
 )
 
 type metricsManager struct {
-	meter         metric.Meter
-	proxyIO       metric.Int64Counter
-	conns         metric.Int64UpDownCounter
-	duration      metric.Int64Histogram
+	meter    metric.Meter
+	proxyIO  metric.Int64Counter
+	conns    metric.Int64UpDownCounter
+	duration metric.Int64Histogram
+
+	distinctClients1m  *distinct.SlidingWindowDistinctCount
+	distinctClients10m *distinct.SlidingWindowDistinctCount
+	distinctClients1h  *distinct.SlidingWindowDistinctCount
+
 	countryLookup geo.CountryLookup
 }
 
@@ -42,15 +49,36 @@ func SetupMetricsManager(geolite2CityURL, cityDBFile string) {
 	}
 
 	// Track connection duration.
-	duration, err := meter.Int64Histogram("proxy.connection_duration", metric.WithDescription("Connection duration"))
+	duration, err := meter.Int64Histogram("proxy.connections.duration", metric.WithDescription("Connection duration"))
 	if err != nil {
 		duration = &noop.Int64Histogram{}
+	}
+
+	// Track number of devices connected over different time windows, using deviceId hashes
+	distinctClients1m := distinct.NewSlidingWindowDistinctCount(time.Minute, time.Second)
+	distinctClients10m := distinct.NewSlidingWindowDistinctCount(10*time.Minute, 10*time.Second)
+	distinctClients1h := distinct.NewSlidingWindowDistinctCount(time.Hour, time.Minute)
+
+	_, err = meter.Int64ObservableGauge(
+		"proxy.clients.active",
+		metric.WithInt64Callback(func(ctx context.Context, io metric.Int64Observer) error {
+			io.Observe(int64(distinctClients1m.Cardinality()), metric.WithAttributes(attribute.String("window", "1m")))
+			io.Observe(int64(distinctClients10m.Cardinality()), metric.WithAttributes(attribute.String("window", "10m")))
+			io.Observe(int64(distinctClients1h.Cardinality()), metric.WithAttributes(attribute.String("window", "1h")))
+			return nil
+		}))
+	if err != nil {
+		log.Warn("failed to create observable gauge for active clients: ", err)
 	}
 
 	metrics.meter = meter
 	metrics.proxyIO = pIO
 	metrics.duration = duration
 	metrics.conns = conns
+
+	metrics.distinctClients1m = distinctClients1m
+	metrics.distinctClients10m = distinctClients10m
+	metrics.distinctClients1h = distinctClients1h
 
 	metrics.countryLookup = geo.FromWeb(geolite2CityURL, cityDBFile, 24*time.Hour, cityDBFile, geo.CountryCode)
 	if metrics.countryLookup == nil {
@@ -70,4 +98,10 @@ func metadataToAttributes(metadata *adapter.InboundContext) []attribute.KeyValue
 		attribute.String("inbound_type", metadata.InboundType),
 		attribute.String("outbound", metadata.Outbound),
 	}
+}
+
+func countDistinctClients(deviceID string) {
+	metrics.distinctClients1m.Add(deviceID)
+	metrics.distinctClients10m.Add(deviceID)
+	metrics.distinctClients1h.Add(deviceID)
 }
